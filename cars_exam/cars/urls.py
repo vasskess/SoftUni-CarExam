c5fe3278fdf5abd4ba9d2c69e778735980c209c4
Vasil <vrangelov85@abv.bg>
2022-11-08 15:20:23 +0200

    fixed last minor issue to cover the requirements

--- cars_exam/cars/urls.py
+++ cars_exam/cars/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from cars_exam.cars.views import *
-
 
 urlpatterns = (
     path("create/", create_car, name="create-car"),
-    path("details/<pk>/", car_details, name="details-car"),
-    path("edit/<pk>/", edit_car, name="edit-car"),
-    path("delete/<pk>/", delete_car, name="delete-car"),
-)
+    path('<int:pk>/', include([
+        path('details/', car_details, name='details-car'),
+        path('edit/', edit_car, name='edit-car'),
+        path('delete/', delete_car, name='delete-car')],)
+    ))
